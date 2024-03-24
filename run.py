164dcc008a2d4e8de6fa2fa0118f3f513586ad2c
joaoghg <jherreiragarnica@gmail.com>
2024-03-24 12:12:11 -0300

    início api

--- run.py
+++ run.py
@@ -1,20 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
+class Enquete(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True, cascade='all,delete')
+    titulo = db.Column(db.String(30), unique=True, nullable=False)
+    descricao = db.Column(db.String(200), nullable=False)
+    data = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    opcoes = db.relationship('EnqueteOpcoes', backref='author', lazy=True, cascade='all,delete')
 
 
-class Post(db.Model):
+class EnqueteOpcoes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    opcao = db.Column(db.String(100), unique=True, nullable=False)
+    enquete_id = db.Column(db.Integer, db.ForeignKey('enquete.id'), nullable=False)
+    votos = db.relationship('Votos', backref='author', lazy=True, cascade='all,delete')
+
+
+class Votos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    enquete_opcoes_id = db.Column(db.Integer, db.ForeignKey('enquete_opcoes.id'), nullable=False)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
